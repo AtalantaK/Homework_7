6c5d4ecb0bb0f36f214f67c16211b5bdf353ca9e
Xenia <aksinia.ra@gmail.com>
2025-06-24 22:40:50 +0500

    Add Tests

--- src/test/java/tests/PlayerServiceTest.java
+++ src/test/java/tests/PlayerServiceTest.java
@@ -3,6 +3,8 @@ package tests;
 import org.homework_7.Player;
 import org.homework_7.PlayerService;
 import org.homework_7.PlayerServiceClass;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import javax.xml.parsers.ParserConfigurationException;
@@ -13,6 +15,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class PlayerServiceTest {
+
+    @AfterEach
+    public void printMessageAfter() {
+        System.out.println("After Each");
+    }
+
+    @BeforeEach
+    public void printMessageBefore(){
+        System.out.println("Before Each");
+    }
+
     @Test
     public void createPlayerWithValidName1() throws IOException, ParserConfigurationException, TransformerException {
         PlayerService serviceJSON = new PlayerServiceClass("JSON");
@@ -21,6 +34,7 @@ public class PlayerServiceTest {
         Player player = serviceJSON.getPlayerById(playerId);
         //1. ER, 2. AR
         assertEquals(playerName, player.getNick());
+        System.out.println("Я тут v1 выполнюсь");
     }
 
     @Test
@@ -31,5 +45,6 @@ public class PlayerServiceTest {
         Player player = serviceJSON.getPlayerById(playerId);
         //1. ER, 2. AR
         assertEquals("playerName", player.getNick()); //специально делаем Failed
+        System.out.println("Я тут v2 не выполнюсь) ");
     }
 }
