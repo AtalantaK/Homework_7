1c841186418265a9734ed66e9645a442752b902d
Xenia <aksinia.ra@gmail.com>
2025-06-24 22:26:47 +0500

    Add Tests

--- src/test/java/tests/PlayerServiceTest.java
+++ src/test/java/tests/PlayerServiceTest.java
@@ -9,15 +9,27 @@ import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerException;
 import java.io.IOException;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class PlayerServiceTest {
     @Test
-    public void createPlayerWithValidName() throws IOException, ParserConfigurationException, TransformerException {
+    public void createPlayerWithValidName1() throws IOException, ParserConfigurationException, TransformerException {
         PlayerService serviceJSON = new PlayerServiceClass("JSON");
         String playerName = "Nick1";
         int playerId = serviceJSON.createPlayer(playerName);
         Player player = serviceJSON.getPlayerById(playerId);
-        System.out.println(player);
+        //1. ER, 2. AR
+        assertEquals(playerName, player.getNick());
+    }
+
+    @Test
+    public void createPlayerWithValidName2() throws IOException, ParserConfigurationException, TransformerException {
+        PlayerService serviceJSON = new PlayerServiceClass("JSON");
+        String playerName = "Nick1";
+        int playerId = serviceJSON.createPlayer(playerName);
+        Player player = serviceJSON.getPlayerById(playerId);
+        //1. ER, 2. AR
+        assertEquals("playerName", player.getNick()); //специально делаем Failed
     }
 }
