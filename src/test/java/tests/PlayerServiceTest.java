e6d03be9f4b8c00429b9f1c61646608476589c96
Xenia <aksinia.ra@gmail.com>
2025-06-30 17:34:09 +0500

    Add Tests

--- src/test/java/tests/PlayerServiceTest.java
+++ src/test/java/tests/PlayerServiceTest.java
@@ -13,38 +13,44 @@ import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
 public class PlayerServiceTest {
 
     @AfterEach
     public void printMessageAfter() {
-        System.out.println("After Each");
+//        System.out.println("After Each");
     }
 
     @BeforeEach
-    public void printMessageBefore(){
-        System.out.println("Before Each");
+    public void printMessageBefore() {
+//        System.out.println("Before Each");
     }
 
     @Test
-    public void createPlayerWithValidName1() throws IOException, ParserConfigurationException, TransformerException {
+    // Создать игрока с валидным именем
+    public void createPlayerWithValidNameTest() throws IOException, ParserConfigurationException, TransformerException {
         PlayerService serviceJSON = new PlayerServiceClass("JSON");
         String playerName = "Nick1";
         int playerId = serviceJSON.createPlayer(playerName);
         Player player = serviceJSON.getPlayerById(playerId);
-        //1. ER, 2. AR
+
+        //Сломать тест
+//        playerName = "Nick2";
+
         assertEquals(playerName, player.getNick());
-        System.out.println("Я тут v1 выполнюсь");
     }
 
     @Test
-    public void createPlayerWithValidName2() throws IOException, ParserConfigurationException, TransformerException {
+    public void getPlayerTest() throws IOException, ParserConfigurationException, TransformerException {
         PlayerService serviceJSON = new PlayerServiceClass("JSON");
         String playerName = "Nick1";
         int playerId = serviceJSON.createPlayer(playerName);
         Player player = serviceJSON.getPlayerById(playerId);
-        //1. ER, 2. AR
-        assertEquals("playerName", player.getNick()); //специально делаем Failed
-        System.out.println("Я тут v2 не выполнюсь) ");
+
+        //Сломать тест
+//        playerId = 100;
+//        playerName = "Nick2";
+
+        assertEquals("Player{id="+playerId+", nick='" + playerName + "', points=0, isOnline=false}", player.toString());
     }
+
 }
